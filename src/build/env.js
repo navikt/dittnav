f3214ba2adef6d86633a74c100f27643868adf23
andrii <andrii.uvarov@nav.no>
2018-06-29 14:19:46 +0200

    IN-375

--- src/build/env.js
+++ src/build/env.js
@@ -1,14 +1,8 @@
-const fsExtra = require('fs-extra');
-
-function createEnvSettingsFile(settingsFile) {
-  fsExtra.ensureFile(settingsFile).then(() => {
-    fsExtra.writeFileSync(
-      settingsFile,
-      `window.appSettings = {
-                REST_API_URL: '${process.env.DITTNAV_API_URL}',
-                LOGIN_URL: '${process.env.LOGINSERVICE_URL}'
-            };`,
-    );
-  });
+function createEnvSettingsFile() {
+  return `window.appSettings = {
+            REST_API_URL: '${process.env.DITTNAV_API_URL}',
+            LOGIN_URL: '${process.env.LOGINSERVICE_URL}'
+        };`;
 }
+
 module.exports = createEnvSettingsFile;
