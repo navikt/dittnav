4214db3fde6b0c3aff9d95880aef9aa8b098b104
Andrii U <un0rsk@users.noreply.github.com>
2018-06-29 11:05:43 +0200

    IN-354 (#3)

--- src/__tests__/components/Lenkelister.test.js
+++ src/__tests__/components/Lenkelister.test.js
@@ -2,9 +2,14 @@ import * as React from 'react';
 import Lenkelister from '../../js/components/Lenkelister';
 const ReactTestRenderer = require('react-test-renderer');
 
-test('basic green component test', () => {
+test('basic green Lenkelister test', () => {
     const component = ReactTestRenderer.create((<Lenkelister links={[]}/>));
     expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('render Lenkelister without links', () => {
+  const component = ReactTestRenderer.create((<Lenkelister />));
+  expect(component.toJSON()).toMatchSnapshot();
 });
 
 test('render a couple of links', () => {
--- src/__tests__/components/Tjenester.test.js
+++ src/__tests__/components/Tjenester.test.js
@@ -7,10 +7,15 @@ test('basic green Tjenester snaphot-test', () => {
     expect(component.toJSON()).toMatchSnapshot();
 });
 
+test('render Tjenester without services', () => {
+  const component = ReactTestRenderer.create((<Tjenester />));
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
 test('render a couple of services', () => {
     const services = [
-        { "url": "https://nav.no/", "title": "Din Pensjon", "description": "Short desc", "imageName": "din_pensjon" },
-        { "url": "https://nav.no/abc", "title": "Meldekort", "description": "Mock desc", "imageName": "registrer_arbeidssoker" },
+        { "url": "https://nav.no/", "title": "Din Pensjon", "description": "Short desc", "className": "din_pensjon" },
+        { "url": "https://nav.no/abc", "title": "Meldekort", "description": "Mock desc", "className": "registrer_arbeidssoker" },
     ];
     const component = ReactTestRenderer.create((<Tjenester services={services} />));
     expect(component.toJSON()).toMatchSnapshot();
