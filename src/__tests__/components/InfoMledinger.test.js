03dd5c4190247bc61be7dc5a96dbca7a6d46565c
andrii <andrii.uvarov@nav.no>
2018-07-04 13:51:42 +0200

    update the test

--- src/__tests__/components/InfoMledinger.test.js
+++ src/__tests__/components/InfoMledinger.test.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import InfoMeldinger from '../../js/components/InfoMeldinger';
 const ReactTestRenderer = require('react-test-renderer');
 
-test('render empty Artikkel', () => {
+test('render empty InfoMeldinger component', () => {
   const component = ReactTestRenderer.create((<InfoMeldinger />));
   expect(component.toJSON()).toMatchSnapshot();
 });
