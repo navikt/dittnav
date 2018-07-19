4bd9f271ddae92c0b1ed9d97ae54190bc97562f5
andrii <andrii.uvarov@nav.no>
2018-07-19 10:54:04 +0200

    tests

--- src/__tests__/components/meldinger/PleiepengerList.test.js
+++ src/__tests__/components/meldinger/PleiepengerList.test.js
@@ -6,3 +6,18 @@ test('empty PleiepengerList green test', () => {
   const component = ReactTestRenderer.create((<PleiepengerList />));
   expect(component.toJSON()).toMatchSnapshot();
 });
+
+test('PleiepengerList empty list', () => {
+  const component = ReactTestRenderer.create((<PleiepengerList pleiepenger={[]} />));
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('PleiepengerList list with one element', () => {
+  const component = ReactTestRenderer.create((<PleiepengerList pleiepenger={[{child: "123321", usedPerToday: "123"}]} />));
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('PleiepengerList list with two elements', () => {
+  const component = ReactTestRenderer.create((<PleiepengerList pleiepenger={[{child: "123321", usedPerToday: "123"}, {child: "123321", usedPerToday: "12"}]} />));
+  expect(component.toJSON()).toMatchSnapshot();
+});
