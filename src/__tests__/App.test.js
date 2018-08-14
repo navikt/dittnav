c47eb10a92b6f04e84b607b34688464ba5b795f4
Andrii U <un0rsk@users.noreply.github.com>
2018-08-14 08:03:22 +0200

    Feature IN-414 / Feilhåndtering (#23)
    * error handling
    * tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,15 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'js/App';
+const ReactTestRenderer = require('react-test-renderer');
 
-it('renders without crashing', () => {
-  const mockApi = {
+const mockApi = () => {
+  return {
     fetchPersonInfoAndServices: () => new Promise((resolve, reject) => {}),
     fetchPaabegynteSaker: () => new Promise((resolve, reject) => {}),
     fetchMinInnboksData: () => new Promise((resolve, reject) => []),
-  };
+  }
+};
+
+const flushPromises = () => {
+  return new Promise(resolve => setImmediate(resolve));
+};
+
+it('renders without crashing', () => {
   const div = document.createElement('div');
 
-  ReactDOM.render(<App api={mockApi} />, div);
+  ReactDOM.render(<App api={mockApi()} />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('expect PaabegynteSoknader fetching', () => {
+  const expectedF = jest.fn();
+  const api = mockApi();
+  api.fetchPaabegynteSaker = () => new Promise((resolve, reject) => {
+    expectedF();
+    return {};
+  });
+
+  const component = ReactTestRenderer.create((<App api={api} />));
+  expect(expectedF).toHaveBeenCalled();
+});
+
+it('expect PaabegynteSoknader fetching', async () => {
+  const api = mockApi();
+  api.fetchPaabegynteSaker = () => new Promise((resolve, reject) => {
+    resolve({feilendeBaksystem: ['hello']});
+  });
+
+  const component = ReactTestRenderer.create((<App api={api} />));
+  await flushPromises();
+
+  expect(component.getInstance().state.errors).toEqual(['Det skjedde en feil under henting av saker']);
+});
+
+it('expect mininnboks fail while fetching', async () => {
+  const api = mockApi();
+  api.fetchMinInnboksData = () => new Promise((resolve, reject) => {
+    reject(new Error('some error'));
+  });
+
+  const component = ReactTestRenderer.create((<App api={api} />));
+  await flushPromises();
+
+  expect(component.getInstance().state.errors).toEqual(['Det skjedde en feil under henting av meldinger fra din innboks']);
 });
--- src/js/Api.js
+++ src/js/Api.js
@@ -1,9 +1,24 @@
-import * as fetch from 'isomorphic-fetch';
 import conf from 'js/Config';
 
-const fetchPersonInfoAndServices = () => fetch(conf.dittNav.DITTNAV_API_URL).then(r => r.json());
-const fetchPaabegynteSaker = () => fetch(conf.dittNav.SAKSOVERSIKT_API_URL).then(r => r.json());
-const fetchMinInnboksData = () => fetch(conf.dittNav.MIN_INNBOKS_URL + conf.MININNBOKS_UBEHANDLET_URL).then(r => r.json());
+const fetchJSONAndCheckForErrors = (url) => {
+  const p = new Promise((res, rej) => {
+    fetch(url) // eslint-disable-line no-undef
+      .then((r) => {
+        if (!r.ok) {
+          rej(new Error('Error happened on requesting a resource'));
+        }
+        res(r.json());
+      })
+      .catch((e) => {
+        rej(e);
+      });
+  });
+  return p;
+};
+
+const fetchPersonInfoAndServices = () => fetchJSONAndCheckForErrors(conf.dittNav.DITTNAV_API_URL);
+const fetchPaabegynteSaker = () => fetchJSONAndCheckForErrors(conf.dittNav.SAKSOVERSIKT_API_URL);
+const fetchMinInnboksData = () => fetchJSONAndCheckForErrors(`${conf.dittNav.MIN_INNBOKS_URL}${conf.MININNBOKS_UBEHANDLET_URL}`);
 
 export default {
   fetchPersonInfoAndServices,
