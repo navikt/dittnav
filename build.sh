01e18f5012ecb57fe8ac2caf677619b16fee5846
andrii <andrii.uvarov@nav.no>
2018-10-17 10:45:52 +0200

    update name to dittnav-nais

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ Gyldige OPTIONS:
 "
 # Default verdier
 v=${versjon}
-IMAGE_NAME="dittnav"
+IMAGE_NAME="dittnav-nais"
 DOCKER_REGISTRY="docker.adeo.no:5000"
 DOCKER_REPOSITORY="personbruker"
 TAG="${DOCKER_REGISTRY}/${DOCKER_REPOSITORY}/${IMAGE_NAME}:${v:="unversioned"}"
