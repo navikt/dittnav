5775a629959f3cc58d771cd79fb0a0dc08b66974
Yrjan Fraschetti <yrjanaff@gmail.com>
2018-12-11 15:40:53 +0100

    Legger til credentials i request til dittnav

--- src/js/Api.js
+++ src/js/Api.js
@@ -2,7 +2,7 @@ import conf from 'js/Config';
 
 const fetchJSONAndCheckForErrors = (url) => {
   const p = new Promise((res, rej) => {
-    fetch(url, { method: 'GET', mode: 'no-cors' }) // eslint-disable-line no-undef
+    fetch(url, { method: 'GET', mode: 'no-cors', credentials: 'include' }) // eslint-disable-line no-undef
       .then((r) => {
         if (r.status === 401 || (r.status === 0 && !r.ok)) {
           window.location.assign(`${conf.dittNav.LOGINSERVICE}?redirect=${window.location.href}`); // eslint-disable-line no-undef
