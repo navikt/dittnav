a4a6718912e258fa2ba24403b7a0bc551df2bb3b
Torbjørn Tessem <torbjorn.tessem@nav.no>
2019-03-21 10:16:08 +0100

    [IN-950] Etterregistrering-beskjed inneholder tallord for ett + laget funksjoner for basetall i i18n

--- src/js/components/EtterregistreringMeldekort.js
+++ src/js/components/EtterregistreringMeldekort.js
@@ -4,21 +4,21 @@ import conf from 'js/Config';
 import i18n from 'translations/i18n';
 import { FormattedMessage as F, injectIntl, intlShape } from 'react-intl';
 
+const tallordForMeldekort = (antallMeldekort, temp) => (antallMeldekort === 1 ? temp.oneNeuter() : temp.numberToWord(antallMeldekort));
+
 class EtterregistreringMeldekort extends Component {
   render() {
     const { ettereg, intl } = this.props;
-    const { numberToWord } = i18n[intl.locale];
     if (ettereg && ettereg.etterregistrerteMeldekort && ettereg.etterregistrerteMeldekort > 0) {
       return (
         <a data-ga="Dittnav/Varsel" className="message clickable meldekort" href={`${conf.dittNav.SERVICES_URL}${conf.ARBEID_PATH}?goto=${conf.ETTERREGISTRERT_PATH}`}>
           <span className="icon meldekort-icon" aria-label="alarm-ikon" />
           <span className="texts">
-            <span>{<F id="meldekort.etterregistreringer" values={{ etterregistreringer: numberToWord(ettereg.etterregistrerteMeldekort) }} />}</span>
+            <span>{<F id="meldekort.etterregistreringer" values={{ etterregistreringer: tallordForMeldekort(ettereg.etterregistrerteMeldekort, i18n[intl.locale]) }} />}</span>
           </span>
         </a>
       );
     }
-
     return null;
   }
 }
