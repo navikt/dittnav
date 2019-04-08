d2bea1213e37a9d4f069b033d1611ba3f37ce01b
Johan Rusvik <johan.rusvik@gmail.com>
2019-04-08 12:18:55 +0200

    Fjerner ubrukt kode

--- src/js/components/Tjenester.js
+++ src/js/components/Tjenester.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { VTA } from '../../spa';
 
 class Tjenester extends Component {
@@ -9,18 +8,4 @@ class Tjenester extends Component {
     );
   }
 }
-
-Tjenester.propTypes = {
-  services: PropTypes.arrayOf(PropTypes.shape({
-    tittel: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-    beskrivelse: PropTypes.string,
-    bildenavn: PropTypes.string,
-  })),
-};
-
-Tjenester.defaultProps = {
-  services: [],
-};
-
 export default Tjenester;
