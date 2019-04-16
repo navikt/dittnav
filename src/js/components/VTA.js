81c7df947fff47dd8f8bae1c368fccfd806cfab8
andrii <andrii.uvarov@nav.no>
2019-04-16 11:08:21 +0200

    * fix lint

--- src/js/components/VTA.js
+++ src/js/components/VTA.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import NAVSPA from '../../NAVSPA';
 import Config from 'js/Config';
+import NAVSPA from '../../NAVSPA';
 
 const VTA = NAVSPA.importer('vta');
 
 class Vta extends React.Component {
   constructor() {
     super();
-    this.state = {render: false};
-    const script = document.createElement("script");
+    this.state = { render: false };
+    const script = document.createElement('script');
     script.src = `${Config.VTA_PATH}/static/js/main.js`;
-    script.type="text/javascript";
+    script.type = 'text/javascript';
     script.addEventListener('load', () => {
-      this.setState({render: true})
+      this.setState({ render: true });
     });
-    document.getElementsByTagName("head")[0].appendChild(script);
-  };
+    document.getElementsByTagName('head')[0].appendChild(script);
+  }
 
   render() {
-    return(
+    return (
       <React.Fragment>
         <link type="text/css" rel="stylesheet" href={`${Config.VTA_PATH}/static/css/main.css`} />
-        { this.state.render ? <VTA/> : null }
+        { this.state.render ? <VTA /> : null }
       </React.Fragment>
     );
   }
