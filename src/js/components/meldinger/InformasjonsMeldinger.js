1cc2bccf64c0edc480f42e2723a5056350e24261
Torbjørn Tessem <torbjorn.tessem@nav.no>
2019-05-16 09:20:43 +0200

    Skrudd av generell infomelding

--- src/js/components/meldinger/InformasjonsMeldinger.js
+++ src/js/components/meldinger/InformasjonsMeldinger.js
@@ -40,7 +40,7 @@ InformasjonsMeldinger.propTypes = {
 
 InformasjonsMeldinger.defaultProps = {
   isMeldeKortUser: false,
-  visGenerellInfo: true,
+  visGenerellInfo: false,
   generellInfo: <F id="generell.informasjonsmelding" />,
   visMeldekortbrukerInfo: false,
   meldekortbrukerInfo: <F id="meldekortbruker.informasjonsmelding" />,
