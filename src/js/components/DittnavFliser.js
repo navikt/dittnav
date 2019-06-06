4218c5279c9a1090d5051d49a94e8dd01d1e69c3
andrii <andrii.uvarov@nav.no>
2019-06-06 10:18:56 +0200

    locale + lenker

--- src/js/components/DittnavFliser.js
+++ src/js/components/DittnavFliser.js
@@ -4,20 +4,20 @@ import Lenkepanel from 'nav-frontend-lenkepanel';
 import { LenkepanelBase } from 'nav-frontend-lenkepanel';
 import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
 import { FormattedMessage } from 'react-intl';
+import Config from '../Config';
 
 class LenkepanelMedIkon extends React.Component {
     render() {
         const {href, onClick, className, overskrift, ingress, children} = this.props;
 
         const linkCreator = (props) => {// eslint-disable-next-line
-            return <a onClick={onClick} {...props}/>;
+            return <a onClick={onClick} {...props} />;
         };
 
         return (
             <LenkepanelBase
                 className={className}
                 href={href}
-                tittelProps="undertittel"
                 linkCreator={linkCreator}
                 border={true}
             >
@@ -44,57 +44,57 @@ class LenkepanelMedIkon extends React.Component {
 }
 
 const Icon = () => (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-<g stroke="#000" strokeLinejoin="round" strokeMiterlimit="10" fill="none">
-    <circle cx="8.5" cy="8.5" r="8"/>
-    <path strokeLinecap="round" d="M14.156 14.156l9.344 9.344"/>
-</g>
-</svg>)
+    <g stroke="#000" strokeLinejoin="round" strokeMiterlimit="10" fill="none">
+        <circle cx="8.5" cy="8.5" r="8"/>
+        <path strokeLinecap="round" d="M14.156 14.156l9.344 9.344"/>
+    </g>
+</svg>);
 
 class DittnavFliser extends React.Component {
     render() {
         return (
         <React.Fragment>
             <div className='dittnav-lenke-container'>
-                <Lenkepanel border={true} className='first'>
-                    <FormattedMessage id='Dine utbetalinger' />
+                <Lenkepanel border={true} className='first' href={`${Config.dittNav.SERVICES_URL}/utbetalingsoversikt/`}>
+                    <FormattedMessage id='fliser.dine.utbetalinger' />
                 </Lenkepanel>
-                <Lenkepanel border={true}>
-                    <FormattedMessage id='Dine saker' />
+                <Lenkepanel border={true} href={`${Config.dittNav.SERVICES_URL}/saksoversikt/`}>
+                    <FormattedMessage id='fliser.dine.saker' />
                 </Lenkepanel>
-                <Lenkepanel border={true} className='last'>
-                    <FormattedMessage id='Innboks' />
+                <Lenkepanel border={true} className='last' href={`${Config.dittNav.SERVICES_URL}/mininnboks/`}>
+                    <FormattedMessage id='fliser.innboks' />
                 </Lenkepanel>
             </div>
             <div className='dittnav-lenkeikon-container'>
                 <LenkepanelMedIkon
-                    href='test'
-                    alt=""
-                    overskrift="ditt-sikevravaer"
+                    alt="Ditt sykefravær"
+                    overskrift="fliser.ditt.sykevravaer"
                     className='first'
+                    href={`${Config.dittNav.SERVICES_URL}/sykefravaer/`}
                 >
                     <Icon/>
                 </LenkepanelMedIkon>
                 <LenkepanelMedIkon
-                    href='test'
-                    alt=""
-                    overskrift="mistet-jobben"
+                    alt="Mistet jobben"
+                    overskrift="fliser.mistet.jobben"
+                    href={`${Config.dittNav.SERVICES_URL}/veiledearbeidssoker/`}
                 >
                     <Icon/>
                 </LenkepanelMedIkon>
             </div>
             <div className='dittnav-lenkeikon-container'>
                 <LenkepanelMedIkon
-                    href='test'
-                    alt=""
-                    overskrift="skjemaer"
+                    alt="Skjemaer"
+                    overskrift="fliser.skjemaer"
                     className='first'
+                    href='/no/person/skjemaer-for-privatpersoner'
                 >
                     <Icon/>
                 </LenkepanelMedIkon>
                 <LenkepanelMedIkon
-                    href='test'
-                    alt=""
-                    overskrift="din-pensjon"
+                    alt="Din pensjon"
+                    overskrift="fliser.din.pensjon"
+                    href={`${Config.dittNav.SERVICES_URL}/pselv/publisering/dinpensjon.jsf`}
                 >
                     <Icon/>
                 </LenkepanelMedIkon>
