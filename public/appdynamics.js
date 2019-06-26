6fc935ff871ca3acb8ad2c2814bceeddc6d45b6a
Asbjørn Steinskog <asbjorn@steinskog.me>
2019-06-26 12:38:32 +0200

    Add appdynamics

---
+++ public/appdynamics.js
@@ -0,0 +1,13 @@
+window['adrum-start-time'] = new Date().getTime();
+(function (config) {
+  var appKey = "";
+  if (window.location.href.indexOf("www-") > -1) {
+    appKey = 'EUM-AAB-AWT';
+  } else {
+    appKey = 'EUM-AAB-AWT';
+  }
+  config.appKey = appKey;
+  config.adrumExtUrlHttps = 'https://jsagent.adeo.no';
+  config.beaconUrlHttps = 'https://eumgw.adeo.no';
+  config.xd = {enable: false};
+})(window['adrum-config'] || (window['adrum-config'] = {}));
