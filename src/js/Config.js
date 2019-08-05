be04d6584499884340a891716d8c4de920e48dce
Sergio Soria <sergio.al.ar@gmail.com>
2019-08-05 22:03:06 +0200

    Sett inn lokale lenker

--- src/js/Config.js
+++ src/js/Config.js
@@ -6,6 +6,20 @@ const getDittNavBaseApiUrl = () => {
   return `https://${window.location.hostname}/person`;
 };
 
+const getUrl = () => {
+  const host = window.location.hostname;
+  if (host.indexOf('localhost') > -1) {
+    return 'https://www-t1.nav.no';
+  }
+  if (host.indexOf('www-q0') > -1) {
+    return 'https://www-q0.nav.no';
+  }
+  if (host.indexOf('www-q1') > -1) {
+    return 'https://www-q1.nav.no';
+  }
+  return 'https://www.nav.no';
+};
+
 const getServicesUrl = () => {
   const host = window.location.hostname;
   if (host.indexOf('localhost') > -1) {
@@ -41,6 +55,39 @@ const getVtaPath = () => {
   return window.location.hostname.indexOf('localhost') > -1 ? 'http://127.0.0.1:3002' : `https://${window.location.hostname}/person/dittnav/veientilarbeid`;
 };
 
+const lenker = {
+  ledigeStillinger: { tittel: 'Ledige stillinger', url: 'https://arbeidsplassen.nav.no/stillinger' },
+  uforetrygd: { tittel: 'Uføretrygd', url: `${getServicesUrl()}/pselv/publisering/uforetrygd.jsf?context=ut` },
+  dineForeldrepenger: { tittel: 'Dine foreldrepenger', url: 'https://foreldrepenger.nav.no' },
+  aktivitetsplan: { tittel: 'Aktivitetsplan', url: `${getServicesUrl()}/aktivitetsplan/` },
+  dinProfil: { tittel: 'Din profil', url: `${getServicesUrl()}/brukerprofil/` },
+  personopplysninger: { tittel: 'Personopplysninger', url: `${getUrl()}/person/personopplysninger` },
+  skjemaer: { tittel: 'Skjemaer', url: `${getUrl()}/no/person/skjemaer-for-privatpersoner` },
+  dinPensjon: { tittel: 'Din pensjon', url: `${getServicesUrl()}/pselv/publisering/dinpensjon.jsf` },
+  dineStillingssok: { tittel: 'Dine stillingssøk', url: 'https://stillingsok.nav.no/pam-stillingsok/lagrede-sok' },
+  registrerDegSomArbeidssoker: { tittel: 'Registrer deg som arbeidssøker', url: `${getServicesUrl()}/veiledearbeidssoker/mistet-jobben/registrering` },
+};
+
+const generelleLenker = [
+  lenker.ledigeStillinger,
+  lenker.uforetrygd,
+  lenker.dineForeldrepenger,
+  lenker.aktivitetsplan,
+  lenker.dinProfil,
+  lenker.registrerDegSomArbeidssoker,
+  lenker.dineStillingssok,
+  lenker.personopplysninger,
+];
+
+const oppfolgingsLenker = [
+  lenker.skjemaer,
+  lenker.dineForeldrepenger,
+  lenker.dinPensjon,
+  lenker.uforetrygd,
+  lenker.dinProfil,
+  lenker.personopplysninger,
+];
+
 export default {
   UNLEASH_TIMEOUT: 3000,
   dittNav: {
@@ -53,6 +100,8 @@ export default {
     REG_STATUS_LINK: 'https://nav.no/sbl/nav_security_check',
     CONTEXT_PATH: '/person/dittnav',
     ARBEIDSGIVER_LOGIN_URL: 'https://www.nav.no/no/Bedrift/Tjenester+og+skjemaer/NAV-+og+Altinn-tjenester',
+    GENERELLE_LENKER: generelleLenker,
+    OPPFOLGINGS_LENKER: oppfolgingsLenker,
   },
   VTA_PATH: getVtaPath(),
   INNLOGGINGSLINJE_AUTH: `${getServicesUrl()}/innloggingslinje-api/auth`,
