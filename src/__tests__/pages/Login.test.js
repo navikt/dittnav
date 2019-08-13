ecdd8410aa1e9b8d425a5e7ee97fb1a8a4d39e40
andrii <andrii.uvarov@nav.no>
2019-08-13 08:23:18 +0200

    fjerner login-side (flyttet til enonic)

--- src/__tests__/pages/Login.test.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import Login from 'js/pages/Login';
-import wrapIntl from 'js/IntlTestHelper';
-const ReactTestRenderer = require('react-test-renderer');
-
-test('Login renders', () => {
-  const component = ReactTestRenderer.create(wrapIntl(<Login />));
-  expect(component.toJSON()).toMatchSnapshot();
-});
