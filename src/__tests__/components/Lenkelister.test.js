3c0380fa55b1f5edf99673f0a02a642cb2f5c87b
Sergio Soria <sergio.al.ar@gmail.com>
2019-08-19 09:49:14 +0200

    Fjern tester

--- src/__tests__/components/Lenkelister.test.js
+++ src/__tests__/components/Lenkelister.test.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Lenkelister from 'js/components/Lenkelister';
-import Config from "../../js/Config";
 const ReactTestRenderer = require('react-test-renderer');
 
 test('basic green Lenkelister test', () => {
@@ -19,15 +18,5 @@ test('render a couple of links', () => {
     { url: '/saksoversikt', tittel: 'Innsyn og oversikt' },
   ];
   const component = ReactTestRenderer.create((<Lenkelister links={links} />));
-  expect(component.toJSON()).toMatchSnapshot();
-});
-
-test('render Lenkelister with fo-links', () => {
-  const component = ReactTestRenderer.create((<Lenkelister links= {Config.dittNav.OPPFOLGINGS_LENKER}/>));
-  expect(component.toJSON()).toMatchSnapshot();
-});
-
-test('render Lenkelister with general-links', () => {
-  const component = ReactTestRenderer.create((<Lenkelister links={Config.dittNav.GENERELLE_LENKER}/>));
   expect(component.toJSON()).toMatchSnapshot();
 });
