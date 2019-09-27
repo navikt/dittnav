536f731c1664b7d2d754c7f88768b562d5edde89
anders-nom <anders@nomerstad.com>
2019-09-27 11:03:18 +0200

    Refaktorerer Config

--- src/js/Config.js
+++ src/js/Config.js
@@ -1,62 +1,68 @@
-const getDittNavBaseApiUrl = () => {
-  const host = window.location.hostname;
-  if (host.indexOf('localhost') > -1) {
-    return process.env.REACT_APP_DITT_NAV_BASE_API_URL;
-  }
-  return `https://${window.location.hostname}/person`;
+const envLabelsToEnvs = {
+  'localhost': 'local',
+  '127': 'local',
+  '': 'local',
+  'www-q0': 'q0',
+  'www-q1': 'q1',
+  'www-q6': 'q6',
+  'www-t1': 't1',
+  'www-t6': 't6',
 };
 
+const getEnvironment = () => {
+  const envLabel = window.location.hostname.split('.', 1)[0];
+  return envLabelsToEnvs[envLabel] ? envLabelsToEnvs[envLabel] : 'prod';
+};
+
+const getDittNavBaseApiUrl = () => (getEnvironment() === 'local'
+  ? process.env.REACT_APP_DITT_NAV_BASE_API_URL
+  : `https://${window.location.hostname}/person`);
+
 const getServicesUrl = () => {
-  const host = window.location.hostname;
-  if (host.indexOf('localhost') > -1) {
+  const ENV = getEnvironment();
+  if (ENV === 'prod') {
+    return 'https://tjenester.nav.no';
+  }
+  if (ENV === 'local') {
     return 'http://localhost:9222';
   }
-  if (host.indexOf('t6') > -1 || host.indexOf('t1') > -1) {
+  if (ENV === 't1' || ENV === 't6') {
     return window.location.origin;
   }
-  if (host.indexOf('www-q0') > -1) {
-    return 'https://tjenester-q0.nav.no';
-  }
-  if (host.indexOf('www-q1') > -1) {
-    return 'https://tjenester-q1.nav.no';
-  }
-  return 'https://tjenester.nav.no';
+  return `https://tjenester-${ENV}.nav.no`;
 };
 
 const getNavUrl = () => {
-  const host = window.location.hostname;
-  if (host.indexOf('localhost') > -1) {
+  const ENV = getEnvironment();
+  if (ENV === 'prod') {
+    return 'https://www.nav.no';
+  }
+  if (ENV === 'local') {
     return 'http://localhost:9222';
   }
-  if (host.indexOf('t6') > -1 || host.indexOf('t1') > -1) {
+  if (ENV === 't1' || ENV === 't6') {
     return window.location.origin;
   }
-  if (host.indexOf('www-q0') > -1) {
-    return 'https://www-q0.nav.no';
-  }
-  if (host.indexOf('www-q1') > -1) {
-    return 'https://www-q1.nav.no';
-  }
-  return 'https://www.nav.no';
+  return `https://www-${ENV}.nav.no`;
 };
 
 const getLoginUrl = () => {
-  const host = window.location.hostname;
-  if (host.indexOf('localhost') > -1) {
-    return 'http://localhost:9111/login?level=Level3';
-  }
-  if (host.indexOf('t6') > -1 || host.indexOf('t1') > -1) {
+  const ENV = getEnvironment();
+  if (ENV === 'prod') {
     return 'https://loginservice.nav.no/login?level=Level3';
   }
-  if (host.indexOf('www-q') > -1) {
-    return 'https://loginservice-q.nav.no/login?level=Level3';
+  if (ENV === 'local') {
+    return 'http://localhost:9111/login?level=Level3';
   }
-  return 'https://loginservice.nav.no/login?level=Level3';
+  if (ENV === 't1' || ENV === 't6') {
+    return 'https://loginservice.nav.no/login?level=Level3';
+  }
+  return 'https://loginservice-q.nav.no/login?level=Level3';
 };
 
-const getVtaPath = () => {
-  return window.location.hostname.indexOf('localhost') > -1 ? 'http://127.0.0.1:3002' : `https://${window.location.hostname}/person/dittnav/veientilarbeid`;
-};
+const getVtaPath = () => (getEnvironment() === 'local'
+  ? 'http://127.0.0.1:3002'
+  : `https://${window.location.hostname}/person/dittnav/veientilarbeid`);
 
 const lenker = {
   ledigeStillinger: { tittel: 'Ledige stillinger', url: 'https://arbeidsplassen.nav.no/stillinger' },
@@ -100,6 +106,7 @@ const oppfolgingsLenker = [
 ];
 
 export default {
+  ENVIRONMENT: getEnvironment(),
   UNLEASH_TIMEOUT: 3000,
   dittNav: {
     SERVICES_URL: getServicesUrl(),
