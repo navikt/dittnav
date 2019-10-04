9f96cdf939d95c81922091f5caebc17391aab497
anders-nom <anders@nomerstad.com>
2019-10-04 16:09:33 +0200

    Endret innloggingslinje url tilbake til opprinnelige

--- src/js/Config.js
+++ src/js/Config.js
@@ -124,7 +124,7 @@ export default {
     OPPFOLGINGS_LENKER: oppfolgingsLenker,
   },
   VTA_PATH: getVtaPath(),
-  INNLOGGINGSLINJE_AUTH: `${getNavUrl()}/innloggingslinje-api/auth`,
+  INNLOGGINGSLINJE_AUTH: `${getServicesUrl()}/innloggingslinje-api/auth`,
   MELDINGER_NAV_PATH: '/sbl/as/minside/meldinger/meldingerNAV.do',
   ARBEID_PATH: '/sbl/nav_security_check',
   MELDEKORT_PATH: '/meldekort/',
