8c1229266bdd7a7b88f0248968fd56d8d8961ed9
Sergio Soria <sergio.al.ar@gmail.com>
2019-10-09 13:38:50 +0200

    Fiks lint

--- src/js/components/DittnavLenkePanel.js
+++ src/js/components/DittnavLenkePanel.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Lenkepanel from 'nav-frontend-lenkepanel';
-import { FormattedMessage as F, FormattedMessage } from 'react-intl';
+import { FormattedMessage as F } from 'react-intl';
 import { Undertittel } from 'nav-frontend-typografi';
 import Config from '../Config';
 
@@ -9,17 +9,17 @@ const DittnavLenkePanel = () => (
   <div className="dittnav-lenke-container blokk-xl">
     <Lenkepanel border className="first" href={Config.LENKER.utbetalingsoversikt.url}>
       <Undertittel className="lenkepanel__heading">
-        <FormattedMessage id="fliser.dine.utbetalinger" />
+        <F id="fliser.dine.utbetalinger" />
       </Undertittel>
     </Lenkepanel>
     <Lenkepanel border href={Config.LENKER.saksoversikt.url}>
       <Undertittel className="lenkepanel__heading">
-        <FormattedMessage id="fliser.dine.saker" />
+        <F id="fliser.dine.saker" />
       </Undertittel>
     </Lenkepanel>
     <Lenkepanel border className="last" href={Config.LENKER.innboks.url}>
       <Undertittel className="lenkepanel__heading">
-        <FormattedMessage id="fliser.innboks" />
+        <F id="fliser.innboks" />
       </Undertittel>
     </Lenkepanel>
   </div>
