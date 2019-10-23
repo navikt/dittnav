42a63d9bcfd65702deae65ad896b5da2406cd69a
Vanessa Kerr <Vanessa.kjolner.kerr@nav.no>
2019-10-23 10:05:43 +0200

    Oppdatert url
    feature/PB-237-Lenke-til-Meldekort

--- src/js/Config.js
+++ src/js/Config.js
@@ -68,7 +68,7 @@ const lenker = {
   uforetrygd: { tittel: 'Uføretrygd', url: `${getServicesUrl()}/pselv/publisering/uforetrygd.jsf?context=ut` },
   dineForeldrepenger: { tittel: 'Dine foreldrepenger', url: 'https://foreldrepenger.nav.no' },
   aktivitetsplan: { tittel: 'Aktivitetsplan', url: `${getServicesUrl()}/aktivitetsplan/` },
-  meldekort: { tittel: 'Meldekort', url: `${getServicesUrl()}/meldekort/om-meldekort` },
+  meldekort: { tittel: 'Meldekort', url: `${getNavUrl()}/meldekort/om-meldekort` },
   personopplysninger: { tittel: 'Personopplysninger', url: `${getNavUrl()}/person/personopplysninger` },
   skjemaer: { tittel: 'Skjemaer', url: `${getNavUrl()}/soknader` },
   dinPensjon: { tittel: 'Din pensjon', url: `${getServicesUrl()}/pselv/publisering/dinpensjon.jsf` },
