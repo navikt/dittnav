7619a0e05a76d04c87318fc178d121fdde04d1ca
anders-nom <anders@nomerstad.com>
2019-10-30 12:18:43 +0100

    Feilsøker 401

--- src/index.js
+++ src/index.js
@@ -26,16 +26,24 @@ function renderApp() {
 }
 
 const checkAuthThenRenderApp = () => {
+  console.log('Checking auth service status...');
   api.checkAuth()
-    .then(api.checkApiStatus)
-    .then(renderApp())
+    .then(() => {
+      console.log('Checking backend api access...');
+      return api.checkApiStatus();
+    })
+    .then(() => {
+      console.log('All auth checks passed, rendering app!');
+      renderApp();
+    })
     .catch((e) => {
       if (Config.ENVIRONMENT === 'local') {
+        console.log('Errors caught, rendering anyway in local environment');
         renderApp();
         return;
       }
       if (e.message === 'not authenticated') {
-        console.log('Not logged inn, redirecting to login service');
+        console.log('Not logged in, redirecting to login service');
         api.redirectToLogin();
         return;
       }
