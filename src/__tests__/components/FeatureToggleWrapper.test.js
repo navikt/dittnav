9efd30bc3f0e70bdb871d1682ecacd4189934ae0
Sissel Fladby <sissel.fladby@nav.no>
2020-01-10 09:37:01 +0100

    Legger til eslint i npm run build for å unngå at de feilene først dukker opp i pipeline

--- src/__tests__/components/FeatureToggleWrapper.test.js
+++ src/__tests__/components/FeatureToggleWrapper.test.js
@@ -7,37 +7,37 @@ const ReactTestRenderer = require('react-test-renderer');
 const C = props => (props.isFeatureEnabled ? <div>Hello, the feature is enabled</div> : <div>Not hello, the feature is disabled</div>);
 
 test('feature is enabled Unleash snaphot-test', async () => {
-  const featureToggles = { "dittnav.fo": true };
+  const featureToggles = { 'dittnav.fo': true };
   const component = ReactTestRenderer.create(
     <FeatureToggles.Provider value={{ featureToggles }}>
       <FeatureToggleWrapper toggle="dittnav.fo">
         <C />
       </FeatureToggleWrapper>
-    </FeatureToggles.Provider>
+    </FeatureToggles.Provider>,
   );
   expect(component.toJSON()).toMatchSnapshot();
 });
 
 test('feature is disabled Unleash snaphot-test', async () => {
-  const featureToggles = { "dittnav.fo": false };
+  const featureToggles = { 'dittnav.fo': false };
   const component = ReactTestRenderer.create(
     <FeatureToggles.Provider value={{ featureToggles }}>
       <FeatureToggleWrapper toggle="dittnav.fo">
         <C />
       </FeatureToggleWrapper>
-    </FeatureToggles.Provider>
+    </FeatureToggles.Provider>,
   );
   expect(component.toJSON()).toMatchSnapshot();
 });
 
 test('no such feature Unleash snaphot-test', async () => {
-  const featureToggles = { "dittnav.notfo": true };
+  const featureToggles = { 'dittnav.notfo': true };
   const component = ReactTestRenderer.create(
     <FeatureToggles.Provider value={{ featureToggles }}>
       <FeatureToggleWrapper toggle="dittnav.fo">
         <C />
       </FeatureToggleWrapper>
-    </FeatureToggles.Provider>
+    </FeatureToggles.Provider>,
   );
   expect(component.toJSON()).toMatchSnapshot();
 });
@@ -48,7 +48,7 @@ test('no set feature toggles', async () => {
       <FeatureToggleWrapper toggle="dittnav.fo">
         <C />
       </FeatureToggleWrapper>
-    </FeatureToggles.Provider>
+    </FeatureToggles.Provider>,
   );
   expect(component.toJSON()).toMatchSnapshot();
 });
