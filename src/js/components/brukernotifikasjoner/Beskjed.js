f8af28b30f0acfcf752a21914e673c60147f720b
sasoria <sergio.alberto.arevalo.soria@nav.no>
2020-04-02 17:53:03 +0200

    Slår midlertidig av beskjeder

--- src/js/components/brukernotifikasjoner/Beskjed.js
+++ src/js/components/brukernotifikasjoner/Beskjed.js
@@ -28,12 +28,11 @@ const onClickBeskjed = (beskjed, dispatch, erAktiv) => {
 };
 
 const Beskjed = ({ beskjed, innlogging, erAktiv, erInaktiv }) => {
+  // Midlertidig fix for beskjeder om forskudd på dagpenger.
+  return null;
+  
   const { dispatch } = useBeskjedStore();
   const sikkerhetsnivaa = useSikkerhetsnivaa(beskjed, 'beskjed', innlogging);
-
-  if (beskjed.tekst.includes('forskudd')) {
-    return null;
-  }
 
   const overskrift = <PanelOverskrift overskrift={sikkerhetsnivaa.tekst} type="Normaltekst" />;
   const lenkeTekst = sikkerhetsnivaa.skalMaskeres ? 'beskjed.lenke.stepup.tekst' : 'beskjed.lenke.tekst';
