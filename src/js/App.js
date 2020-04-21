a7294e555daffef3c5001da59a0a2c2f862e7ff7
sasoria <sergio.alberto.arevalo.soria@nav.no>
2020-04-21 19:42:17 +0200

    Bruker render for å sende med props i en Route

--- src/js/App.js
+++ src/js/App.js
@@ -9,32 +9,48 @@ import '../less/index.less';
 import ApiType from './types/ApiType';
 
 const App = ({ api }) => (
-  <Router>
+  <Router basename="/person">
     <Switch>
-      <Route path="/person/dittnav" exact>
-        <BeskjedStoreProvider>
-          <RenderHome api={api} />
-        </BeskjedStoreProvider>
-      </Route>
-      <Route path="/person/dittnav/varslinger">
-        { Config.VARSLINGER_FEATURE_TOGGLE ? (
+      <Route
+        path="/dittnav"
+        exact
+        render={(props) => (
           <BeskjedStoreProvider>
-            <RenderVarslinger api={api} />
+            <RenderHome {...props} api={api} />
           </BeskjedStoreProvider>
-        ) : null }
-      </Route>
-      <Route path="/person/dittnav/hendelser">
-        { Config.TEST_SIDE_FEATURE_TOGGLE ? (
+        )}
+      />
+      {Config.VARSLINGER_FEATURE_TOGGLE && (
+        <Route
+          path="/dittnav/varslinger"
+          exact
+          render={(props) => (
+            <BeskjedStoreProvider>
+              <RenderVarslinger {...props} api={api} />
+            </BeskjedStoreProvider>
+          )}
+        />
+      )}
+      {Config.TEST_SIDE_FEATURE_TOGGLE && (
+        <Route
+          path="/dittnav/hendelser"
+          exact
+          render={(props) => (
+            <BeskjedStoreProvider>
+              <HendelserTestSide {...props} />
+            </BeskjedStoreProvider>
+          )}
+        />
+      )}
+      <Route
+        path="/dittnav*"
+        exact
+        render={(props) => (
           <BeskjedStoreProvider>
-            <HendelserTestSide />
+            <RenderHome {...props} api={api} />
           </BeskjedStoreProvider>
-        ) : null }
-      </Route>
-      <Route path="/person/dittnav/*">
-        <BeskjedStoreProvider>
-          <RenderHome api={api} />
-        </BeskjedStoreProvider>
-      </Route>
+        )}
+      />
     </Switch>
   </Router>
 );
