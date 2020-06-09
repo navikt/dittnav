fd5d78b8d780d3d87f9b018fb46c3123a52da7cd
sasoria <sergio.alberto.arevalo.soria@nav.no>
2020-06-09 11:14:01 +0200

    Egen komponent for scrollToTop

--- src/js/utils/scroll.js
+++ src/js/utils/scroll.js
@@ -2,17 +2,12 @@ export const trimHashSign = (hash) => (
   hash.replace('#', '')
 );
 
-const scrollToTop = () => (
-  window.scroll(0, 0)
-);
-
 const scroll = (hash) => {
   setTimeout(() => {
     const id = trimHashSign(hash);
     const element = document.getElementById(id);
 
     if (element) {
-      scrollToTop();
       element.scrollIntoView({
         behavior: 'smooth',
       });
