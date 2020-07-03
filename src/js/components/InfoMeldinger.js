6f2e0841079b109074cab473eaf0bb7aec726623
sasoria <sergio.alberto.arevalo.soria@nav.no>
2020-07-03 11:31:18 +0200

    Skrur midlertidig av et endepunkt for å sjekke om det har en effekt på cpu-bruk i database-serveren

--- src/js/components/InfoMeldinger.js
+++ src/js/components/InfoMeldinger.js
@@ -19,7 +19,7 @@ import InnboksType from '../types/InnboksType';
 const InfoMeldinger = (props) => {
   const { state } = useBeskjedStore();
   const isMeldeKortUser = props.meldekort ? props.meldekort.meldekortbruker : false;
-  const visInngangTilVarslinger = props.antallBrukernotifikasjoner > 0;
+  // const visInngangTilVarslinger = props.antallBrukernotifikasjoner > 0;
 
   return (
     <section className="infomeldinger-list">
@@ -35,7 +35,7 @@ const InfoMeldinger = (props) => {
       <EtterregistreringMeldekort ettereg={props.meldekort} />
       <PaabegynteSoknader paabegynteSoknader={props.paabegynteSoknader} />
       <MinInnboks mininnboks={props.mininnboks} />
-      {visInngangTilVarslinger ? <InngangVarslinger /> : null}
+      <InngangVarslinger />
     </section>
   );
 };
