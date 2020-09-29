29c90a3015b7c7a95c720826ea640578df58cdbe
sasoria <sergio.alberto.arevalo.soria@nav.no>
2020-09-29 09:36:10 +0200

    Endret FeilMeldinger til en functional component

--- src/js/components/FeilMeldinger.js
+++ src/js/components/FeilMeldinger.js
@@ -1,22 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage as F } from 'react-intl';
 
-class FeilMeldinger extends Component {
-  render() {
-    return (
-      <>
-        {this.props.errors.length > 0 && (
-          <section className="service-notification">
-            <h1 className="typo-element">
-              {this.props.errors.map(e => (<span key={e}><F id={e} /></span>))}
-            </h1>
-          </section>
-        )}
-      </>
-    );
-  }
-}
+const FeilMeldinger = (props) => (
+  <>
+    {props.errors.length > 0 && (
+      <section className="service-notification">
+        <h1 className="typo-element">
+          {props.errors.map(e => (<span key={e}><F id={e} /></span>))}
+        </h1>
+      </section>
+    )}
+  </>
+);
 
 FeilMeldinger.propTypes = {
   errors: PropTypes.arrayOf(PropTypes.string),
