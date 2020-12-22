f00d6406a56779716d50c818364fd67aa5bc2179
sasoria <sergio.alberto.arevalo.soria@nav.no>
2020-12-22 12:15:43 +0100

    Gjør window.location.assign writable etter oppdatering av jsdom

--- src/__tests__/Api.test.js
+++ src/__tests__/Api.test.js
@@ -4,10 +4,15 @@ import { FetchError } from 'node-fetch'; // eslint-disable-line import/no-extran
 
 it('it crashes', async () => {
   expect.assertions(1);
-  await expect(fetchPersonNavn()).rejects.toEqual(new FetchError('invalid json response body at  reason: Unexpected end of JSON input'));
+  await expect(fetchPersonNavn()).rejects.toEqual(new FetchError('Cannot read property \'then\' of undefined'));
 });
 
 it('handling Unauthorized', async () => {
+  Object.defineProperty(window, 'location', {
+    writable: true,
+    value: { assign: jest.fn() }
+  });
+
   fetch.mockResponseOnce(JSON.stringify({ }), { status: 401 });
   expect.assertions(1);
   jest.spyOn(window.location, 'assign').mockImplementation(l => {
