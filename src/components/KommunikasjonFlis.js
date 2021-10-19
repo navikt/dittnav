ef003ad35382ff9e8973f53f6950029a328c51d6
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2021-10-19 13:43:43 +0200

    Fjernet onClick på KommunikasjonFlisElement da den ikke triggeres.

--- src/components/KommunikasjonFlis.js
+++ src/components/KommunikasjonFlis.js
@@ -10,54 +10,37 @@ import IkonBrevVedtak from '../assets/IkonBrevVedtak';
 
 import KommunikasjonFlisElement from './KommunikasjonFlisElement';
 import { lenker } from '../utils/lenker';
-import { GoogleAnalyticsAction, GoogleAnalyticsCategory, trackEvent } from '../utils/googleAnalytics';
 
 const KommunikasjonFlis = ({ oppfolging }) => (
   <PanelBase className="kommunikasjon-flis">
-  
     <Systemtittel className="kommunikasjon-flis-overskrift">
       <F id="kommunikasjon.med.nav.overskrift" />
     </Systemtittel>
 
     <KommunikasjonFlisElement
-      className="kommunikasjon-flis-innboks" 
-      tittel={<F id="kommunikasjon.med.nav.innboks" />} 
+      className="kommunikasjon-flis-innboks"
+      tittel={<F id="kommunikasjon.med.nav.innboks" />}
       undertekst={<F id="kommunikasjon.med.nav.innboks.undertekst" />}
       Ikon={IkonEmail}
       linkUrl={lenker.nyInnboks.url}
-      onClick={() => trackEvent(
-        GoogleAnalyticsCategory.Forside,
-        GoogleAnalyticsAction.DineSaker,
-        lenker.nyInnboks.url,
-      )}
     />
-    {oppfolging 
-      ? (
-        <KommunikasjonFlisElement
-          className="kommunikasjon-flis-dialog" 
-          tittel={<F id="kommunikasjon.med.nav.dialog.veileder" />} 
-          undertekst={<F id="kommunikasjon.med.nav.dialog.veileder.undertekst" />}
-          Ikon={IkonDialogMedVeileder}
-          linkUrl={lenker.dialogMedVeileder.url}
-          onClick={() => trackEvent(
-            GoogleAnalyticsCategory.Forside,
-            GoogleAnalyticsAction.DineSaker,
-            lenker.dialogMedVeileder.url,
-          )}         
-        />
-      )
-      : ''}
-    <KommunikasjonFlisElement 
-      className="kommunikasjon-flis-brev-vedtak" 
-      tittel={<F id="kommunikasjon.med.nav.brev.vedtak" />}  
+    {oppfolging ? (
+      <KommunikasjonFlisElement
+        className="kommunikasjon-flis-dialog"
+        tittel={<F id="kommunikasjon.med.nav.dialog.veileder" />}
+        undertekst={<F id="kommunikasjon.med.nav.dialog.veileder.undertekst" />}
+        Ikon={IkonDialogMedVeileder}
+        linkUrl={lenker.dialogMedVeileder.url}
+      />
+    ) : (
+      ''
+    )}
+    <KommunikasjonFlisElement
+      className="kommunikasjon-flis-brev-vedtak"
+      tittel={<F id="kommunikasjon.med.nav.brev.vedtak" />}
       undertekst={<F id="kommunikasjon.med.nav.brev.vedtak.undertekst" />}
       Ikon={IkonBrevVedtak}
       linkUrl={lenker.minInnboks.url}
-      onClick={() => trackEvent(
-        GoogleAnalyticsCategory.Forside,
-        GoogleAnalyticsAction.DineSaker,
-        lenker.minInnboks.url,
-      )}      
     />
   </PanelBase>
 );
