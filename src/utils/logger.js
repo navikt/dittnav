6a19bee720f4ec630c4cd4147292cee36535bd3a
sasoria <sergio.alberto.arevalo.soria@nav.no>
2021-11-11 14:34:25 +0100

    Setter opp sentry

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/browser';
 import { Toggle } from '../constants';
 
 const log = (message) => {
@@ -5,6 +6,13 @@ const log = (message) => {
     // eslint-disable-next-line no-console
     console.log(message);
   }
+};
+
+export const initializeSentry = () => {
+  Sentry.init({
+    dsn: 'https://075601a148b14886a5bfdc8372b9b8cd@sentry.gc.nav.no/106',
+    autoSessionTracking: false,
+  });
 };
 
 export default log;
