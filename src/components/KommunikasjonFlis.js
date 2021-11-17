51dd34cc161579abac9669a57db592686f567f16
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2021-11-17 14:31:08 +0100

    La til amplitude logging av lenker i kommunikasjon-med-nav flisen.

--- src/components/KommunikasjonFlis.js
+++ src/components/KommunikasjonFlis.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Systemtittel } from 'nav-frontend-typografi';
 import { FormattedMessage as F } from 'react-intl';
 import PanelBase from 'nav-frontend-paneler';
+import { Systemtittel } from 'nav-frontend-typografi';
 
 import IkonEmail from '../assets/IkonEmail';
 import IkonDialogMedVeileder from '../assets/IkonDialogMedVeileder';
 import IkonBrevVedtak from '../assets/IkonBrevVedtak';
 
 import KommunikasjonFlisElement from './KommunikasjonFlisElement';
+import { listOfActions, listOfComponentNames, logAmplitudeEvent } from '../utils/amplitudeUtils';
 import { lenker } from '../utils/lenker';
 
 const KommunikasjonFlis = ({ oppfolging }) => (
@@ -19,6 +20,7 @@ const KommunikasjonFlis = ({ oppfolging }) => (
 
     <KommunikasjonFlisElement
       className="kommunikasjon-flis-innboks"
+      onClick={() => logAmplitudeEvent(listOfComponentNames.KommunikasjonFlis.Innboks, listOfActions.TrykkPaaLenke)}
       tittel={<F id="kommunikasjon.med.nav.innboks" />}
       undertekst={<F id="kommunikasjon.med.nav.innboks.undertekst" />}
       Ikon={IkonEmail}
@@ -27,6 +29,7 @@ const KommunikasjonFlis = ({ oppfolging }) => (
     {oppfolging ? (
       <KommunikasjonFlisElement
         className="kommunikasjon-flis-dialog"
+        onClick={() => logAmplitudeEvent(listOfComponentNames.KommunikasjonFlis.DialogMedLokalVeileder, listOfActions.TrykkPaaLenke)}
         tittel={<F id="kommunikasjon.med.nav.dialog.veileder" />}
         undertekst={<F id="kommunikasjon.med.nav.dialog.veileder.undertekst" />}
         Ikon={IkonDialogMedVeileder}
@@ -37,6 +40,7 @@ const KommunikasjonFlis = ({ oppfolging }) => (
     )}
     <KommunikasjonFlisElement
       className="kommunikasjon-flis-brev-vedtak"
+      onClick={() => logAmplitudeEvent(listOfComponentNames.KommunikasjonFlis.BrevOgVedtak, listOfActions.TrykkPaaLenke)}
       tittel={<F id="kommunikasjon.med.nav.brev.vedtak" />}
       undertekst={<F id="kommunikasjon.med.nav.brev.vedtak.undertekst" />}
       Ikon={IkonBrevVedtak}
