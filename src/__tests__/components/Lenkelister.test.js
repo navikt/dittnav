72754454b1f6083a6e7898c52988506d60589077
Eilertsen <eivinn.eilertsen@nav.no>
2021-11-23 15:13:14 +0100

    Fjernet en blank linje i lenkelister.test slik at Github actionen passerer.

--- src/__tests__/components/Lenkelister.test.js
+++ src/__tests__/components/Lenkelister.test.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Lenkelister from 'components/Lenkelister';
 const ReactTestRenderer = require('react-test-renderer');
 
-
 test('basic green Lenkelister test', () => {
   const component = ReactTestRenderer.create((<Lenkelister links={[]} />));
   expect(component.toJSON()).toMatchSnapshot();
