b4bfed81a0cea2b8ca904fbe7303bb1cbe0a9c7e
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-01-10 11:05:32 +0100

    Legger til informelding om årsrapporten

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { FormattedMessage as F } from 'react-intl';
 import { Undertittel } from 'nav-frontend-typografi';
 import { useIsFetching } from 'react-query';
+import AlertStripe from 'nav-frontend-alertstriper';
+import Lenke from 'nav-frontend-lenker';
 import { useOppfolging } from '../../hooks/usePerson';
-import { generelleLenker, oppfolgingsLenker } from '../../utils/lenker';
+import { generelleLenker, lenker, oppfolgingsLenker } from '../../utils/lenker';
 import useStore from '../../hooks/useStore';
 import PersonInfo from '../../components/PersonInfo';
 import InfoMeldinger from '../../components/InfoMeldinger';
@@ -15,8 +17,8 @@ import InnloggingsModal from '../../components/common/InnloggingsModal';
 import Brodsmuler from '../../utils/brodsmuler';
 import Vta from '../../components/VTA';
 import PageBase from '../PageBase';
-import DittnavFliser from '../../components/DittnavFliser'; 
-import UnleashWrapper from '../../components/UnleashWrapper'
+import DittnavFliser from '../../components/DittnavFliser';
+import UnleashWrapper from '../../components/UnleashWrapper';
 
 const Home = () => {
   const [{ data: oppfolging, isLoading: oppfolgingIsLoading, isError: oppfolgingIsError }] = useOppfolging();
@@ -45,9 +47,13 @@ const Home = () => {
         <div className="maincontent side-innhold">
           <div className="col-md-12" id="dittnav-main-container">
             <PersonInfo />
+            <AlertStripe type="info" className="rapport-info">
+              <b>Leter du etter årsoppgaven din?</b><br />
+              Hvis du har valgt digital postkasse finner du årsoppgaven hos Digipost eller eBoks. Ellers finner du den i <Lenke href={lenker.minInnboks.url}>innboksen</Lenke> hos NAV. Dokumenter som er sendt til deg digitalt vil du i tillegg kunne finne igjen blant <Lenke href={lenker.mineSaker}>sakene dine</Lenke>.
+            </AlertStripe>
             {isFetching ? <DelayedSpinner delay={500} spinnerClass="header-spinner" /> : null}
             <InfoMeldinger />
-            <NyVtaForStandardInnsats isLoading={oppfolgingIsLoading} isError={oppfolgingIsError}/>
+            <NyVtaForStandardInnsats isLoading={oppfolgingIsLoading} isError={oppfolgingIsError} />
             <KoronaSpesial />
             <DittnavLenkePanel />
             {oppfolgingIsLoading ? null : generelleEllerVta}
