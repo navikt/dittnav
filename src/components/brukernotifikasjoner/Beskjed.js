9827dbb38d761ba6c1d19aa60ddccfdf7b723401
eivinne <eivinn_e@hotmail.com>
2022-03-21 11:18:13 +0100

    Endret logikk slik at det gamle utseendet blir applied for beskjeder uten lenke, samt at POST-logikk for done eventene er tilpasset for beskjeder med og uten lenke

--- src/components/brukernotifikasjoner/Beskjed.js
+++ src/components/brukernotifikasjoner/Beskjed.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { bool } from 'prop-types';
 import useSikkerhetsnivaa from '../../hooks/useSikkerhetsnivaa';
 import useStore from '../../hooks/useStore';
@@ -10,8 +10,9 @@ import InnloggingsstatusType from '../../types/InnloggingsstatusType';
 import BeskjedType from '../../types/BeskjedType';
 import { listOfActions, listOfComponentNames, logAmplitudeEvent } from '../../utils/amplitudeUtils';
 import LenkepanelMedIkon from '../common/LenkepanelMedIkon';
+import PanelMedIkon from '../common/PanelMedIkon';
 
-const remove = (beskjed, removeBeskjed) => {
+const remove = (beskjed, removeBeskjed, harIkkeLenke) => {
   if (beskjed.produsent === 'digiSos') {
     postDigisosDone({
       eventId: beskjed.eventId,
@@ -24,6 +25,10 @@ const remove = (beskjed, removeBeskjed) => {
     });
   }
 
+  if (!harIkkeLenke) {
+    return;
+  }
+
   removeBeskjed(beskjed);
 };
 
@@ -33,39 +38,74 @@ const addTilInaktiveHvisErAktiv = (beskjed, addInaktivBeskjed, erAktiv) => {
   }
 };
 
-const Beskjed = ({ beskjed, innloggingsstatus, erAktiv }) => {
+const Beskjed = ({ beskjed, innloggingsstatus, erAktiv, erInaktiv }) => {
   const { removeBeskjed, addInaktivBeskjed, visInnloggingsModal } = useStore();
+  const [isBeingRemoved, setIsBeingRemoved] = useState(false);
 
   const sikkerhetsnivaa = useSikkerhetsnivaa(beskjed, 'beskjed', innloggingsstatus);
   const lokalDatoTid = transformTolokalDatoTid(beskjed.eventTidspunkt);
+  const lenkeTekst = sikkerhetsnivaa.skalMaskeres ? 'beskjed.lenke.stepup.tekst' : 'beskjed.lenke.tekst';
+  const harIkkeLenke = beskjed.link === null || beskjed.link === "";
   const overskrift = <PanelOverskrift overskrift={sikkerhetsnivaa.tekst} type="Element" />;
   const overskriftInnlogging = <PanelOverskrift overskrift={sikkerhetsnivaa.tekst} type="LoginLenke" />;
-  
+
+  const visKnapp = !(sikkerhetsnivaa.skalMaskeres || erInaktiv);
+
   const handleArkiverBeskjed = (skalMaskeres) => {
     if (skalMaskeres) {
       logAmplitudeEvent(listOfComponentNames.brukernotifikasjon.BeskjedMottatt, listOfActions.LoggInn);
       return;
     }
 
-    remove(beskjed, removeBeskjed, visInnloggingsModal);
+    remove(beskjed, removeBeskjed, harIkkeLenke);
     addTilInaktiveHvisErAktiv(beskjed, addInaktivBeskjed, erAktiv);
     logAmplitudeEvent(listOfComponentNames.brukernotifikasjon.BeskjedMottatt, listOfActions.TrykkPaaArkiverKnapp);
   };
-  
+
+  const onClickBeskjed = () => {
+    setIsBeingRemoved(true);
+  };
+
+  const onAnimationEnd = () => {
+    handleArkiverBeskjed();
+  };
   return (
-    <LenkepanelMedIkon
-      className="beskjed"
-      alt="Beskjed"
-      overskrift={sikkerhetsnivaa.skalMaskeres ? overskriftInnlogging : overskrift}
-      etikett={lokalDatoTid}
-      href={sikkerhetsnivaa.lenke}
-      sikkerhetsnivaa={sikkerhetsnivaa}
-      onClick={handleArkiverBeskjed}
-      amplitudeAction={listOfActions.TrykkPaaBrukernotifikasjon}
-      amplitudeComponentName={listOfComponentNames.brukernotifikasjon.BeskjedMottatt}
-    >
-      <IkonBeskjed />
-    </LenkepanelMedIkon>
+    <>
+      { !harIkkeLenke 
+        ? (
+          <LenkepanelMedIkon
+            className="beskjed"
+            alt="Beskjed"
+            overskrift={sikkerhetsnivaa.skalMaskeres ? overskriftInnlogging : overskrift}
+            etikett={lokalDatoTid}
+            href={sikkerhetsnivaa.lenke}
+            sikkerhetsnivaa={sikkerhetsnivaa}
+            onClick={handleArkiverBeskjed}
+            amplitudeAction={listOfActions.TrykkPaaBrukernotifikasjon}
+            amplitudeComponentName={listOfComponentNames.brukernotifikasjon.BeskjedMottatt}
+          >
+            <IkonBeskjed />
+          </LenkepanelMedIkon>
+        )
+        : (
+          <PanelMedIkon
+            className={isBeingRemoved ? 'remove beskjed' : 'beskjed'}
+            alt="Beskjed"
+            overskrift={sikkerhetsnivaa.tekst}
+            etikett={lokalDatoTid}
+            onClick={() => onClickBeskjed()}
+            onAnimationEnd={onAnimationEnd}
+            skjermleserTekst="beskjed.knapp.skjermleser.tekst"
+            lenke={sikkerhetsnivaa.lenke}
+            lenkeTekst={lenkeTekst}
+            amplitudeAction={listOfActions.TrykkPaaBrukernotifikasjon}
+            amplitudeComponentName={listOfComponentNames.brukernotifikasjon.BeskjedMottatt}
+            knapp={visKnapp}
+          >
+            <IkonBeskjed />
+          </PanelMedIkon>
+        )}
+    </>
   );
 };
 
