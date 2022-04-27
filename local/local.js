835f4062b8af0efdd6c9b32e771eb638b6710210
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-04-27 13:24:11 +0200

    Rydder bort mocking av mininnboks

--- local/local.js
+++ local/local.js
@@ -38,10 +38,6 @@ app.get('/dittnav-api/ident', (req, res) => (
   res.sendFile(path.resolve(__dirname, './mock/ident.json'))
 ));
 
-app.get('/dittnav-api/meldinger/ubehandlede', (req, res) => (
-  res.sendFile(path.resolve(__dirname, './mock/ubehandlede.json'))
-));
-
 app.get('/dittnav-api/saker', (req, res) => (
   res.sendFile(path.resolve(__dirname, './mock/saker.json'))
 ));
